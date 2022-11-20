37697debea9088a6f44be95cd69dd6b8ccf312a8
Hershell Koger <hersheykoger@yahoo.com>
2022-11-20 08:27:19 -0500

    Wrap all the class names in '' for appendChild methods

--- src/home.js
+++ src/home.js
@@ -1,25 +1,24 @@
 export default function createHomePage() {
     const content = document.querySelector('#content');
     const header = document.createElement('header');
-    header.classList.add(header)
+    header.classList.add('header')
     header.textContent = "Eden's Sweets";
-    content.appendChild(content);
+    content.appendChild(header);
 
     const tabContainer = document.createElement('div');
-    tabContainer.classList.add(tab-container);
+    tabContainer.classList.add('tab-container');
     content.appendChild(tabContainer);
 
     const centerPane = document.createElement('div');
-    centerPane.classList.add(center-pane);
+    centerPane.classList.add('center-pane');
     content.appendChild(centerPane);
 
     const description = document.createElement('h3');
-    description.classList.add(description);
+    description.classList.add('description');
     description.textContent = "So what is it?"
     centerPane.appendChild(description);
 
     const descriptionParagraph = document.createElement('p');
     descriptionParagraph.textContent = "Lorem Ipsum";
     centerPane.appendChild(descriptionParagraph);
-
 }
