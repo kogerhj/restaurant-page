116229fb714b663f87fe78fd64306ded3b77c1f7
Hershell Koger <hersheykoger@yahoo.com>
2022-11-21 08:31:43 -0500

    Start work on changeTab()

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@ import createContactsPage from "./contacts.js";
 
 createHomePage();
 
+function changeTabs() {
+    let tabButtons = document.querySelectorAll('.btn');
+    for (let i = 0; i < tabButtons.length; i++) {
+        tabButtons[i].addEventListener('click', () => console.log("it works"))
+    }
+}
+
+changeTabs();
+
+
 
