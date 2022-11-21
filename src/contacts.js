a435de8a5be50aa08a7c321ac35bb7f1618a2503
Hershell Koger <hersheykoger@yahoo.com>
2022-11-21 07:50:03 -0500

    Copy and paste logic from home.js to work tab-changing logic

---
+++ src/contacts.js
@@ -0,0 +1,42 @@
+export default function createContactsPage() {
+    const content = document.querySelector('#content');
+    const header = document.createElement('header');
+    header.classList.add('header')
+    header.textContent = "Eden's Sweets";
+    content.appendChild(header);
+
+    const tabContainer = document.createElement('div');
+    tabContainer.classList.add('tab-container');
+    content.appendChild(tabContainer);
+
+    function createTabs(num) {
+        let tabNames = ['Home', 'Menu', 'Contact'];
+        let btnClasses = ['home', 'menu', 'contact'];
+        for (let i = 0; i < num; i++) {
+            let tab = document.createElement('button');
+            tab.classList.add('btn');
+            tab.classList.add(btnClasses[i]);
+            tab.textContent = tabNames[i];
+            tabContainer.appendChild(tab);
+        }
+    }
+
+    createTabs(3);
+
+    const centerPane = document.createElement('div');
+    centerPane.classList.add('center-pane');
+    content.appendChild(centerPane);
+
+    const description = document.createElement('h3');
+    description.classList.add('description');
+    description.textContent = "So what is it?"
+    centerPane.appendChild(description);
+
+    const descriptionParagraph = document.createElement('p');
+    descriptionParagraph.textContent = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+        ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+        ugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim 
+        id est laborum.`;
+    centerPane.appendChild(descriptionParagraph);
+}
