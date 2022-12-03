5454c7c48681d2c67a90b93274911f5b2e995300
Hershell Koger <hersheykoger@yahoo.com>
2022-12-02 22:52:20 -0500

    Import landing.js module

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
+import createLandingPage from "./landing.js";
 import createHomePage from "./home.js";
 import createMenuPage from "./menu.js";
 import createContactsPage from "./contacts.js";
 
-createHomePage();
+createLandingPage();
 
-function changeTabs() {
-    let tabButtons = document.querySelectorAll('.btn');
-    for (let i = 0; i < tabButtons.length; i++) {
-        tabButtons[i].addEventListener('click', () => console.log("it works"))
-    }
-}
 
-changeTabs();
+
+
+
 
 
 
