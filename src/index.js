c134b6b7c8aad5a9f62341b90ecc58f003689782
Hershell Koger <hersheykoger@yahoo.com>
2022-12-04 16:09:16 -0500

    Import createLandingPage()

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import createLandingPage from "./landing.js";
-import createHomeTab from "./home.js";
-import createMenuTab from "./menu.js";
-import createContactsTab from "./contacts.js";
+import createHomeTab from "./home";
+import createMenuTab from "./menu";
+import createContactsTab from "./contacts";
 
 createLandingPage();
+
 
 
 
